91c8648295c180535ccf74f1eccba1acf0f5331d
Marc985 <hei.marc.3@gmail.com>
2024-07-23 08:43:50 +0300

    chore:add patrimoine code artifact

--- src/main/java/com/harena/com/PojaApplication.java
+++ src/main/java/com/harena/com/PojaApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @PojaGenerated
 public class PojaApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(PojaApplication.class, args);
   }
