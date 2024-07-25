b2e05f35eff57906ed2ea9918aa6aeb53752187a
Tsiory <hei.tsiory@gmail.com>
2024-07-25 14:16:35 +0300

    refractor: rename writeBase64ToTxt -> writeToTxt

--- src/main/java/com/harena/com/services/SerializationFunctions.java
+++ src/main/java/com/harena/com/services/SerializationFunctions.java
@@ -1,24 +1,22 @@
 package com.harena.com.services;
 
-import com.harena.com.file.BucketComponent;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.hei.patrimoine.modele.Patrimoine;
 import school.hei.patrimoine.serialisation.Serialiseur;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.util.Base64;
+
 @Service
 public class SerializationFunctions {
     Serialiseur<Patrimoine> serialiseur = new Serialiseur<>();
 
     public File serialize(Patrimoine patrimoine) throws IOException {
         String base64String = serialiseur.serialise(patrimoine);
-        return writeBase64ToTxt(base64String , patrimoine.nom()+".txt");
+        return writeToTxt(base64String , patrimoine.nom()+".txt");
     }
 
-    private File writeBase64ToTxt(String base64String, String filePath) {
+    public File writeToTxt(String base64String, String filePath) {
         File file = new File(filePath);
         try (FileWriter writer = new FileWriter(filePath)) {
              writer.write(base64String);
