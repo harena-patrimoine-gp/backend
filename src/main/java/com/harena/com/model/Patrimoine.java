c44f6614576dbfe019fa854e5e845e14a0a05072
Marc985 <hei.marc.3@gmail.com>
2024-07-28 14:33:51 +0300

    fix:add defautl constructor

--- src/main/java/com/harena/com/model/Patrimoine.java
+++ src/main/java/com/harena/com/model/Patrimoine.java
@@ -4,11 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import school.hei.patrimoine.modele.Personne;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 @Data
-@AllArgsConstructor
-public class Patrimoine {
+public class Patrimoine implements Serializable {
     private  String nom;
-    private Personne personne;
+    private Personne possesseur;
     private LocalDate t;
 }
