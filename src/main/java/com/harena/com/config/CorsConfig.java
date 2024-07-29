b9379304fad745b1f3bcd058119ddd05d998aec2
Marc985 <hei.marc.3@gmail.com>
2024-07-29 08:29:49 +0300

    chore:add deployed url into the allowed origin

--- src/main/java/com/harena/com/config/CorsConfig.java
+++ src/main/java/com/harena/com/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173","https://frontend-patrimoine.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "Accept");
 
