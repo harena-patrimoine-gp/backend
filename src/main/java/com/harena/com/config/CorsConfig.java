c58a3082f8d20fb84f7d0f2fccf204bb2d8ba7a2
Marc985 <hei.marc.3@gmail.com>
2024-10-14 21:27:28 +0300

    chore:add front url to cors

--- src/main/java/com/harena/com/config/CorsConfig.java
+++ src/main/java/com/harena/com/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","https://frontend-patrimoine.vercel.app","https://harena-patrimoine-ui.vercel.app")
+                .allowedOrigins("http://localhost:3000","https://frontend-patrimoine.vercel.app","https://harena-patrimoine-ui.vercel.app","https:patrimoine-three.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "Accept");
 
